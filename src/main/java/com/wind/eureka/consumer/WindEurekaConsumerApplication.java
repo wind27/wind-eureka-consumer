3cf56da3b1cccf5961dcef00352e8b3cbed69e8b
qianchun <qianchun@huli.com>
2017-10-25 14:48:19 +0800

    init

--- src/main/java/com/wind/eureka/consumer/WindEurekaConsumerApplication.java
+++ src/main/java/com/wind/eureka/consumer/WindEurekaConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.wind.eureka.consumer.main;
+package com.wind.eureka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
